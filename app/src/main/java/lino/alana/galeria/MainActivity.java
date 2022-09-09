b54ee9c0caee8140d3574d24491ac70cf19968e1
20201tiimi0276 <alanasousalino@gmail.com>
2022-09-09 12:54:35 -0300

    até o passo 6 completo: siga do passo 7 adiante

--- app/src/main/java/lino/alana/galeria/MainActivity.java
+++ app/src/main/java/lino/alana/galeria/MainActivity.java
@@ -3,6 +3,9 @@ package lino.alana.galeria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+        //Setando o elemento tbMain (ToolBar) como a ActionBar padrão do MainActivity.
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar);
+
+        //criando as opções de menu da activity
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            super.onCreateOptionsMenu(menu);
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.main_activity_tb, menu);
+            return true;
+        }
+
+
+
+
+
+            }
 }
--- app/src/main/java/lino/alana/galeria/PhotoActivity.java
+++ app/src/main/java/lino/alana/galeria/PhotoActivity.java
@@ -1,8 +1,12 @@
 package lino.alana.galeria;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        //definindo o tbPhoto como padrão do PhotoActivity
+        Toolbar toolbar = findViewById(R.id.tbPhoto);
+        setSupportActionBar(toolbar);
+
+        //habilitando um botão que volta para a página Main Activity
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            super.onCreateOptionsMenu(menu);
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.main_activity_tb, menu);
+            return true;
+        }
+
+
+
+
     }
 }
