bad40144147ecb62ee44ab599045251db1e5f54e
20201tiimi0276 <alanasousalino@gmail.com>
2022-12-15 10:58:22 -0300

    Atividade concluída

--- app/src/main/java/lino/alana/galeria/PhotoActivity.java
+++ app/src/main/java/lino/alana/galeria/PhotoActivity.java
@@ -1,14 +1,21 @@
 package lino.alana.galeria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.core.motion.utils.Utils;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 public class PhotoActivity extends AppCompatActivity {
+
+    String photoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,14 @@ public class PhotoActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        Intent i = getIntent();
+        photoPath = i.getStringExtra("photo_path");
+
+        Bitmap bitmap = Utils.getBitmap(photoPath);
+        ImageView imPhoto = findViewById(R.id.imPhoto);
+        imPhoto.setImageBitmap(bitmap);
+
+
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
             super.onCreateOptionsMenu(menu);
@@ -30,6 +45,29 @@ public class PhotoActivity extends AppCompatActivity {
             inflater.inflate(R.menu.main_activity_tb, menu);
             return true;
         }
+
+        public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.opShare:
+                    sharePhoto();
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+
+        void sharePhoto() {
+            // Codigo para cpmpartiilhar a foto
+            Uri photoUri = FileProvider.getUriForFile(PhotoActivity.this, "trindade.daniel.galeria.fileprovider", new File(photoPath));
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.putExtra(Intent.EXTRA_STREAM, photoUri);
+            i.setType("image/jpeg");
+            startActivity(i);
+        }
+
+
+
+
 
 
 
